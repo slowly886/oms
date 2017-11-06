127c8c0a49e0c7c19cd2c0538c9c6aa71d10269d
zhouchengcheng <zhoucc9303@163.com>
2017-11-06 14:40:05 +0800

    修改测试类名

--- src/main/java/com/develop/oms/controller/TestController.java
+++ src/main/java/com/develop/oms/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * To change this template use File | Settings | Editor | File and Code Templates.
  **/
 @RestController
-public class HelloController {
+public class TestController {
 
     @RequestMapping("/hello")
     public String hello() {
